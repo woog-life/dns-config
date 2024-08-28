b46908ca9df3e3ee1aa128f4f515d9407ae8501f
Björn Petersen <git@bjoernpetersen.net>
2024-08-28 20:10:09 +0200

    Remove CF token handling

--- github_actions.tf
+++ github_actions.tf
@@ -31,26 +31,6 @@ resource "google_service_account_key" "github_actions" {
   service_account_id = google_service_account.github_actions_access.account_id
 }
 
-# Cloudflare access
-
-data "cloudflare_api_token_permission_groups" "all" {}
-
-resource "cloudflare_api_token" "github_actions" {
-  name = "GitHub Actions woog-dns"
-
-  policy {
-    permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
-      data.cloudflare_api_token_permission_groups.all.zone["Zone Read"],
-      data.cloudflare_api_token_permission_groups.all.user["API Tokens Read"],
-    ]
-    resources = {
-      "com.cloudflare.api.account.zone.${var.zone_id}" = "*",
-      "com.cloudflare.api.user.a2f5f7a2a192da0c6afa7efe597d7a8d" : "*"
-    }
-  }
-}
-
 # GitHub secrets
 
 data "github_repository" "terraform_repo" {
@@ -61,10 +41,4 @@ resource "github_actions_secret" "google" {
   repository      = data.github_repository.terraform_repo.name
   secret_name     = "GOOGLE_SA_JSON"
   plaintext_value = base64decode(google_service_account_key.github_actions.private_key)
-}
-
-resource "github_actions_secret" "cloudflare" {
-  repository      = data.github_repository.terraform_repo.name
-  secret_name     = "CLOUDFLARE_TOKEN"
-  plaintext_value = cloudflare_api_token.github_actions.value
 }
